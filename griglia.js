b459b4bfa09090f5951aa831d6222b971319a3a3
AndreaBartoloni <andrea.bartoloni00@gmail.com>
2023-11-08 10:14:19 +0100

    last commit

--- griglia.js
+++ griglia.js
@@ -1,28 +1,52 @@
-
-// Numeri di celle
-
 let play = document.getElementById("play");
-let numCella = 100;
+
 let grid = document.getElementById("grid");
 
+play.addEventListener("click", function(){
+    let difficult = document.getElementById("select").value;
+    let square = "";
+    grid.replaceChildren()
+    
+    console.log(difficult);
+    if(difficult == "medium"){
+        numCella = 81;
+        square = "square81";
+        console.log("stronzo");
+        
+    }else if(difficult == "hard"){
+        numCella = 49; 
+        square = "square49";
+        console.log("stronzo");
+    }else{
+        numCella = 100;
+        square = "square";
+        console.log("stronzo");
+    }
+    creaQuadrati(numCella, square);
+})
 
+function creaQuadrati(numeroCella, cssSquare){
+    
+    for(let i = 1; i <= numeroCella; i++){
+            
+        let celle = document.createElement("div");
+        celle.classList.add(cssSquare);
+        celle.innerHTML = i;
+        grid.appendChild(celle); 
 
-function creaQuadrati(){
+        
+    
+        celle.addEventListener("click", function(){
+            celle.classList.toggle("highlight");
+            console.log("Numero della casella", i)
+        });
+        
+    }  
+    
+    
+     
+        
    
-    play.addEventListener("click", function(){
-        for(let i = 1; i <= numCella; i++){
-            
-            let celle = document.createElement("div");
-            celle.classList.add("square");
-            grid.appendChild(celle); 
-        
-            celle.addEventListener("click", function(){
-                celle.classList.toggle("highlight");
-                console.log("Numero della casella", i)
-            });
-            
-        }
-        
-    })
+    
+    
 }
-creaQuadrati();
